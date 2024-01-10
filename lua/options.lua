6d616bb24d4b9ef01978bfac2bdfffd263624d93
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-10 11:41:53 -0600

    Add assorted keymaps from old config

--- lua/options.lua
+++ lua/options.lua
@@ -22,3 +22,34 @@ vim.g.mapleader = " "
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 vim.keymap.set("n", "<Leader><Space>", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
 vim.keymap.set("n", "-", "<CMD>lua MiniFiles.open()<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<Escape>", "<CMD>nohlsearch<CR>", { desc = "Clear search highlights" })
+
+-- copied from old config
+local opts = { noremap = true, silent = true }
+
+-- move highlights up and down
+vim.keymap.set("v", "<pagedown>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<pageup>", ":m '<-2<CR>gv=gv")
+
+-- keep cursor positon when folding lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- use leader + arrows to move windows
+vim.keymap.set("n", "<leader><left>", "<C-w>h", opts)
+vim.keymap.set("n", "<leader><down>", "<C-w>j", opts)
+vim.keymap.set("n", "<leader><up>", "<C-w>k", opts)
+vim.keymap.set("n", "<leader><right>", "<C-w>l", opts)
+
+-- stay in visual mode when indenting
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- intuitive increment/decrement
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+
+-- select all lines in a file
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- "move" buffers
+vim.keymap.set("n", "<leader>m", "<cmd>BufferLinePick<cr>")
