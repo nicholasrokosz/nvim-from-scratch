651f8be05782f01dd68c1e359f409a2b5de4d13f
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-12 23:04:24 -0600

    Configure bufferline offset

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -40,7 +40,18 @@ return {
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons',
         config = function()
-            require("bufferline").setup()
+            require("bufferline").setup({
+                options = {
+                    offsets = {
+                        {
+                            filetype = "neo-tree",
+                            text = "File Explorer",
+                            text_align = "center",
+                            separator = true
+                        }
+                    }
+                }
+            })
         end
     },
     -- {
