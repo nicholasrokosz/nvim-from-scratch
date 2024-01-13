776cff00e247efd5ed52bc01252a32eee6955473
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-12 22:50:38 -0600

    Add lualine and bufferline

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -35,19 +35,27 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {},
     },
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup()
+        end
+    },
     -- {
-    --     'romgrk/barbar.nvim',
-    --     dependencies = {
-    --         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-    --         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    --     },
-    --     init = function() vim.g.barbar_auto_setup = false end,
-    --     opts = {
-    --         sidebar_filetypes = {
-    --             ['neo-tree'] = {event = 'BufWipeout'},
-    --         },
-    --         letters = 'neiotsramgNEIOTSRAMG',
-    --     },
-    --     version = '^1.0.0', -- optional: only update when a new 1.x version is released
-    -- },
-}
+        --     'romgrk/barbar.nvim',
+        --     dependencies = {
+            --         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            --         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+            --     },
+            --     init = function() vim.g.barbar_auto_setup = false end,
+            --     opts = {
+                --         sidebar_filetypes = {
+                    --             ['neo-tree'] = {event = 'BufWipeout'},
+                    --         },
+                    --         letters = 'neiotsramgNEIOTSRAMG',
+                    --     },
+                    --     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+                    -- },
+                }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,6 +8,9 @@ return {
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+        })
         vim.keymap.set("n", "<Leader>e", "<cmd>Neotree toggle<cr>", { desc = "Clear search highlights" })
     end
 }
