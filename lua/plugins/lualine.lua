f35ea9443a6e4b002a0f78f8be293785df459b0b
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-13 23:05:34 -0600

    Use global statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,11 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup()
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                globalstatus = false,
+            },
+        })
+    end,
 }
