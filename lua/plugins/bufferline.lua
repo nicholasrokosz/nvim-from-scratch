61f79a9fe0b692d350048415ca47fa8d8f9e0e4c
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-13 23:25:47 -0600

    Configure bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+return {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        local bufferline = require("bufferline")
+        bufferline.setup({
+            options = {
+                style_preset = bufferline.style_preset.minimal,
+                show_buffer_close_icons = false,
+                offsets = {
+                    {
+                        filetype = "neo-tree",
+                        text = "File Explorer",
+                        text_align = "center",
+                        separator = true,
+                    },
+                },
+            },
+        })
+    end,
+}
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -35,25 +35,6 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {},
     },
-    {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require("bufferline").setup({
-                options = {
-                    offsets = {
-                        {
-                            filetype = "neo-tree",
-                            text = "File Explorer",
-                            text_align = "center",
-                            separator = true
-                        }
-                    }
-                }
-            })
-        end
-    },
     -- {
         --     'romgrk/barbar.nvim',
         --     dependencies = {
