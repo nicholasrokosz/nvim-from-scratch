fbf2cb002f2b7b8fad3a58f423cdba4809aa7ac8
Nick Rokosz <nicholasrokosz@gmail.com>
2024-01-15 22:51:37 -0600

    Add toggleterm and configure for lazygit

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+
+        function Lazygit_toggle()
+            lazygit:toggle()
+        end
+
+        vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
+    end,
+}
